d42166757df9c4afd0c034751bf54f4ede0d6a72
vwandalka <vwandalka@gmail.com>
2018-09-29 15:18:00 +0200

    zadanie 3

--- js/app.js
+++ js/app.js
@@ -16,22 +16,35 @@ document.addEventListener('DOMContentLoaded', function() {
     
 //zadanie 2
     
-    var buttons = document.querySelectorAll('.read-more'); 
-    
-    function showHide() {
-        var textArea = this.previousElementSibling; 
-        if (textArea.style.display === 'none' || textArea.style.display === '') {
-            textArea.style.display = 'block';
-        this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
-      } else {
-           textArea.style.display = 'none';
-        this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
-      }
-    }
-    
-    for (var i = 0; i <= buttons.length; i++) {
-        buttons[i].addEventListener('click', showHide); 
+    var buttons = document.querySelectorAll('.read-more');
 
+  function showHide() {
+
+    var textArea = this.previousElementSibling;
+   
+    if (textArea.style.display === 'none' || textArea.style.display === '') {
+      textArea.style.display = 'block';
+      this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+    } else {
+      textArea.style.display = 'none';
+      this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
     }
+  }
+
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', showHide);
+  }
+
+
+//zadanie 3
+
+    var menu = document.querySelector('.navbar');
     
-});
+    window.addEventListener('scroll', function() {
+      menu.style.borderBottom = '1px solid #7f7f7f';
+    });
+                            
+                      
+//zadanie 4
+    
+                            });
