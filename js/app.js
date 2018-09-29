037bc44cbf2ff1f6f538672d4325e0f0c23f8034
vwandalka <vwandalka@gmail.com>
2018-09-29 14:47:43 +0200

    commit 4

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict'; 
 
+//zadanie 1
+
 document.addEventListener('DOMContentLoaded', function() {
      var menuButton = document.querySelector('.for-dropdown');
      var subMenu = document.querySelector('.dropdown');
@@ -11,4 +13,25 @@ document.addEventListener('DOMContentLoaded', function() {
     menuButton.addEventListener('mouseout', function() {
     subMenu.style.display = 'none';
     })
-    });
+    
+//zadanie 2
+    
+    var buttons = document.querySelectorAll('.read-more'); 
+    
+    function showHide() {
+        var textArea = this.previousElementSibling; 
+        if (textArea.style.display === 'none' || textArea.style.display === '') {
+            textArea.style.display = 'block';
+        this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+      } else {
+           textArea.style.display = 'none';
+        this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+      }
+    }
+    
+    for (var i = 0; i <= buttons.length; i++) {
+        buttons[i].addEventListener('click', showHide); 
+
+    }
+    
+});
